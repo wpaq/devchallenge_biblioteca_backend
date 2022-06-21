b9255a43fd9c4b0bb0c1bebe2cec27cff07d0c61
wpaq <wallysonpabloo@gmail.com>
2022-06-21 15:42:28 -0300

    ensure return 500 if no httpRequest is provided

--- src/routers/biblioteca-router.spec.js
+++ src/routers/biblioteca-router.spec.js
@@ -1,5 +1,11 @@
 class BibliotecaRouter {
   route (httpRequest) {
+    if (!httpRequest) {
+      return {
+        statusCode: 500
+      }
+    }
+
     if (!httpRequest.body.title) {
       return {
         statusCode: 400
@@ -16,5 +22,11 @@ describe('Biblioteca Router', () => {
     }
     const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 500 if no httpRequest is provided', async () => {
+    const sut = new BibliotecaRouter()
+    const httpResponse = await sut.route()
+    expect(httpResponse.statusCode).toBe(500)
   })
 })
